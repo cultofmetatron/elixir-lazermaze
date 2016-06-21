f91fb9d9acd6d6ffc377429844cabca3fc9cccc5
Peter de Croos <cultofmetatron@aumlogic.com>
2016-06-21 16:45:39 -0700

    extracting the loaded data

--- lib/maze_loader.ex
+++ lib/maze_loader.ex
@@ -6,4 +6,36 @@ defmodule MazeLoader do
     {:ok, file} = File.read file_path
     file
   end
+
+  @doc """
+    takes an array of mirrors and returns a list of mirrors with x, y and type
+    iex> process_mirrors([["4", "5", "/"], ["2", "4", "\"])
+    [{x: 4, y: 5, m: "/"}, {x: 2, y: 4 m: "\\"}]
+  """
+  def process_mirrors(mirrors) do
+    
+  end
+
+  @doc """
+    Takes the contents and returns a maze spec
+  """
+  def extract_data(contents) do
+    [dim | [ strt | mirrors ]] = data = String.split(contents, "\n")
+      |> Enum.filter(fn(str) -> str !== "" end)
+      |> Enum.map(fn(str)-> String.split(str, ~r/\s/) end)
+    size = %{ 
+      length: dim |> hd |> Integer.parse |> elem(0),
+      width: dim |> tl |> hd |> Integer.parse |> elem(0)
+    }
+    start = %{
+      x: strt |> hd |> Integer.parse |> elem(0),
+      y: strt |> tl |> hd |> Integer.parse |> elem(0),
+      direction: hd(tl(tl(strt)))
+    }
+   %{
+      size: size,
+      start: start
+    }
+  end
+
 end
--- test/maze_loader_test.exs
+++ test/maze_loader_test.exs
@@ -10,8 +10,15 @@ defmodule MazeLoaderTest do
   test "it loads the raw file" do
     file = MazeLoader.load_raw("./samples-data/basic.dat")
     file_contents = "5 6\n1 4 S\n3 4 \/\n3 0 \/\n1 2 \\\n3 2 \\\n4 3 \\\n"
-    IO.puts file_contents
     assert file == file_contents
+  end
+
+  test "it returns a processed file" do
+    file = MazeLoader.load_raw("./samples-data/basic.dat")
+    data = MazeLoader.extract_data(file)
+    IO.inspect(data)
+    assert data == %{size: %{length: 5, width: 6}, start: %{direction: "S", x: 1, y: 4}}
+
   end
 
   
